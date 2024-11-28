277ff0e4cbe617f1e90ed0f4f512c071260a8821
Tanjo <2403627832@qq.com>
2024-11-28 17:50:18 +0800

    add quicksort.c

--- c/trial/quicksort.c
+++ c/trial/quicksort.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 #include "mine.h"
 
+void quicksort(int arr[], int start, int end);
+
 int main() {
     int *arr = getintArray();
+    int n = _msize((void*)arr)/sizeof(arr[0]);
+    quicksort(arr, 0, n-1);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     free((void*)arr);
     return 0;
 }
 
-void quicksort(int array[], int low, int high) {
-    int i = low; 
-    int j = high;
-    if(i >= j) {
+void quicksort(int arr[], int l, int r) {//l(left)为排序起始位置，r(right)为排序终止位置，全部排序设置l=0,r=n-1
+    int i = l, j = r;
+    int base = arr[l];//设置基准值
+    if (l >= r) {
         return;
     }
- 
-    int temp = array[low];
-    while(i != j) {
-        while(array[j] >= temp && i < j) {
+    while (i < j) {
+        while(j > i) {
+            if (arr[j] < base) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                break;
+            }
             j--;
         }
-	while(array[i] <= temp && i < j) {
+        while (i < j) {
+            if (arr[i] > base) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                break;
+            }
             i++;
         }
-	if(i < j) {
-            swap(array[i], array[j]);
-        }
     }
- 
-    //将基准temp放于自己的位置，（第i个位置）
-    swap(array[low], array[i]);
-    QuickSort(array, low, i - 1);
-    QuickSort(array, i + 1, high);
+    quicksort(arr, l, i - 1);
+    quicksort(arr, i + 1, r);
 }
