7099526f5b5bef547ec3c65be37b3a0cd5803112
Tanjo <2403627832@qq.com>
2025-01-17 01:04:16 +0800

    provisional gluttonous snake game

--- c_cpp/trial/gluttonous_snake_provisional.cpp
+++ c_cpp/trial/gluttonous_snake_provisional.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 
+void gotoxy(int x, int y);
+
 class Snake {
 private:
     vector<pair<int, int>> body;
@@ -33,22 +35,38 @@ public:
         do {
             fruit.first = rand() % (WIDTH - 1);
             fruit.second = rand() % (HEIGHT - 1);
-        } while (find(body.begin(), body.end(), fruit) != body.end() && fruit != make_pair(0, 0));
+        } while (find(body.begin(), body.end(), fruit) != body.end() || (fruit.first == 0 || fruit.second == 0));
     }
 
     void input() {
         if (_kbhit()) {
             switch (_getch()) {
             case 'a':
+                if (body.begin() + 2 == body.end() && dir == RIGHT) {
+                    gameOver = true;
+                    break;
+                }
                 dir = LEFT;
                 break;
             case 'd':
+                if (body.begin() + 2 == body.end() && dir == LEFT) {
+                    gameOver = true;
+                    break;
+                }
                 dir = RIGHT;
                 break;
             case 'w':
+                if (body.begin() + 2 == body.end() && dir == DOWN) {
+                    gameOver = true;
+                    break;
+                }
                 dir = UP;
                 break;
             case 's':
+                if (body.begin() + 2 == body.end() && dir == UP) {
+                    gameOver = true;
+                    break;
+                }
                 dir = DOWN;
                 break;
             case 'x':
@@ -98,23 +116,37 @@ public:
         body.insert(body.begin(), head);
     }
 
-    void draw() {
-        system("cls");
+    void map() {
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
                 if (i == 0 || i == HEIGHT - 1)
-                    cout << "-";
+                    cout << '-';
                 else if (j == 0 || j == WIDTH - 1)
-                    cout << "|";
-                else if (find(body.begin(), body.end(), make_pair(j, i)) != body.end())
-                    cout << "o";
-                else if (j == fruit.first && i == fruit.second)
-                    cout << "бя";
-                else
-                    cout << " ";
+                    cout << '|';
+                else 
+                    cout << ' ';
             }
             cout << endl;
         }
+    }
+
+    void content() {
+        gotoxy(1, 1);
+        for (int i = 1; i < HEIGHT - 1; i++) {
+            for (int j = 1; j < WIDTH - 1; j++) {
+                cout << ' ';
+                if (find(body.begin(), body.end(), make_pair(j, i)) != body.end()) {
+                    gotoxy(j, i);
+                    cout << 'o';
+                }
+                else if (make_pair(j, i) == fruit) {
+                    gotoxy(j, i);
+                    cout << 'F';
+                }
+            }
+            gotoxy(1, i + 1);
+        }
+        gotoxy(0, HEIGHT);
         cout << "Score: " << score << endl;
     }
 
@@ -126,8 +158,10 @@ public:
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
     Snake snake;
+    system("cls");
+    snake.map();
     while (!snake.isGameOver()) {
-        snake.draw();
+        snake.content();
         snake.input();
         snake.logic();
         Sleep(100);
@@ -135,4 +169,9 @@ int main() {
     cout << "Game Over!" << endl;
     Sleep(7000);
     return 0;
+}
+
+void gotoxy(int x, int y) {
+    COORD pos = { (short)x, (short)y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
